f242de402e5f0d68b41bc2a317b98b08c875fb75
toz00 <44646289+toz00@users.noreply.github.com>
2020-12-08 19:31:33 -0300

    Update neuralstyle.py

--- neuralstyle.py
+++ neuralstyle.py
@@ -6,9 +6,9 @@ from keras.preprocessing.image import load_img, save_img, img_to_array
 import matplotlib.pyplot as plt
 from keras.applications import vgg19
 from keras.models import Model
-#from keras import optimizers
-from scipy.optimize import fmin_l_bfgs_b
-#from keras.applications.vgg19 import VGG19
+
+vgg19
+
 #vgg19_weights = '../input/vgg19/vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5'
 #vgg19 = VGG19(include_top = False, weights=vgg19_weights)
 
@@ -22,22 +22,3 @@ style_image_path = 'model.jpg'
 width, height = load_img(base_image_path).size
 img_nrows = 400
 img_ncols = int(width * img_nrows / height)
-
-
-def preprocess_image(image_path):
-    from keras.applications import vgg19
-    img = load_img(image_path, target_size=(img_nrows, img_ncols))
-    img = img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = vgg19.preprocess_input(img)
-    return img
-
-plt.figure()
-plt.title("Base Image",fontsize=20)
-img1 = load_img(base_image_path)
-plt.imshow(img1)
-
-plt.figure()
-plt.title("Style Image",fontsize=20)
-img1 = load_img(style_image_path)
-plt.imshow(img1)
